de77f7293508c4705244c42ddea8f21c7e28d280
IanGonzalezTDF <102770923+IanGonzalezTDF@users.noreply.github.com>
2022-06-05 18:33:13 -0300

    Resultado

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 let lista1: number[] = new Array(6);
 let lista2: number[] = new Array(6);
 let suma: number[] = new Array(6);
-let indice: number;
+let contador: number;
 
-for (indice = 0; indice < 6; indice++) {
-  lista1[indice] = Number(
-    prompt(`Ingrese el numero a guardar en la posicion ${indice} de la LISTA 1`)
+for (contador = 0; contador < 6; contador++) {
+  lista1[contador] = Number(
+    prompt(
+      `Ingrese el numero a guardar en la posicion ${contador} de la LISTA 1`
+    )
   );
 }
-for (indice = 0; indice < 6; indice++) {
-  lista2[indice] = Number(
-    prompt(`Ingrese el numero a guardar en la posicion ${indice} de la LISTA 2`)
+for (contador = 0; contador < 6; contador++) {
+  lista2[contador] = Number(
+    prompt(
+      `Ingrese el numero a guardar en la posicion ${contador} de la LISTA 2`
+    )
   );
 }
-for (indice = 0; indice < 6; indice++) {
-  suma[indice] = lista1[indice] + lista2[indice];
-  console.log(suma[indice]);
+for (contador = 0; contador < 6; contador++) {
+  suma[contador] = lista1[contador] + lista2[contador];
+  console.log(`El resultado de la suma es : ${suma[contador]}`);
 }
