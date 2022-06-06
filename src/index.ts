897e71082c3a248aa6179bc8d1026cbaf17621d8
IanGonzalezTDF <102770923+IanGonzalezTDF@users.noreply.github.com>
2022-06-06 14:30:22 -0300

    detalles+1

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,22 @@ let contador: number;
 for (contador = 0; contador < 6; contador++) {
   lista1[contador] = Number(
     prompt(
-      `Ingrese el numero a guardar en la posicion ${contador} de la LISTA 1`
+      `Ingrese el numero a guardar en la posicion ${contador + 1} de la LISTA 1`
     )
   );
 }
 for (contador = 0; contador < 6; contador++) {
   lista2[contador] = Number(
     prompt(
-      `Ingrese el numero a guardar en la posicion ${contador} de la LISTA 2`
+      `Ingrese el numero a guardar en la posicion ${contador + 1} de la LISTA 2`
     )
   );
 }
 for (contador = 0; contador < 6; contador++) {
   suma[contador] = lista1[contador] + lista2[contador];
-  console.log(`El resultado de la suma es : ${suma[contador]}`);
+  console.log(
+    `El resultado de la suma de la posicion ${contador + 1} es : ${
+      suma[contador]
+    }`
+  );
 }
